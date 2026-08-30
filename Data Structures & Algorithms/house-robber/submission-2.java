0e4c176dc18e6b5c43792e9871b0067edf149830
Marcus Franklin J <marcus.franklin.j12@gmail.com>
2026-08-30 20:19:56 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        
+        if(len ==0)return 0;
+        if(len == 1)return nums[0];
+        
+
+
+        if(len >2)nums[2]+=nums[0];
+
+        for(int i = 3 ; i < len ; i++){
+            nums[i] += Math.max(nums[i-2],nums[i-3]);
+        }
+        return Math.max(nums[len-1],nums[len-2]);
+    }
+}
