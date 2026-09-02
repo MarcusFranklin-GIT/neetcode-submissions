ab406e7e17a4c0e30f1c55c14612064e7fdce7ec
Marcus Franklin J <marcus.franklin.j12@gmail.com>
2026-09-02 19:27:53 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+
+        int len = nums.length;
+
+        int newnums[] = new int[len+2];
+
+        for(int i = 0 ; i < len ; i++){
+            newnums[i+1] = nums[i];
+        }
+
+        newnums[0] = 1;
+        newnums[len+1] = 1;
+
+        int dp[][] = new int[len+2][len+2];
+        for(int row[] : dp)Arrays.fill(row,-1);
+
+        return burst(1,len,newnums,dp);
+        
+    }
+
+    private int burst(int left , int right , int[] nums,int dp[][]){
+        
+        if(left > right)return 0;
+
+        if(dp[left][right] != -1) return dp[left][right];
+
+        int maxi = Integer.MIN_VALUE;
+
+        for(int ind = left ; ind <= right ; ind++){
+            
+            int call1 = burst(left,ind-1,nums,dp);
+            int call2 = burst(ind+1,right,nums,dp);
+            
+            int cost = (nums[left-1] * nums[ind] * nums[right+1])+ call1 + call2;
+
+            maxi = Math.max(maxi,cost);
+        }
+
+        return dp[left][right] = maxi;
+    }
+}
+
+
+
+
+
+
