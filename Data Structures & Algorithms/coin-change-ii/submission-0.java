1a7f3027dd3bd744e64f0d7270acd3b840b77aa0
Marcus Franklin J <marcus.franklin.j12@gmail.com>
2026-09-02 10:11:19 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        int ways[] = new int[amount+1];
+
+        ways[0] =1;
+
+        for(int coin : coins){
+            for(int i = coin; i <= amount ; i++){
+                if(i>=coin){
+                    ways[i] += ways[i-coin];
+                }
+            }
+        }
+        return ways[amount];
+
+    }
+}
